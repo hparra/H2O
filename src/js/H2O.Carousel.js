3243ae73d044b753fb87f598ce36682a4b3994c0
luoa <luoa@uci.edu>
2009-07-08 22:33:27 -0700

    H2O.Carousel almost fully functional
    H2O.Carousel is H2O compliant and scales MxN

--- src/js/H2O.Carousel.js
+++ src/js/H2O.Carousel.js
@@ -1,74 +1,61 @@
-var rows = 2;
-var columns = 2;
-var JSON;
-
-function FCarouselHeight() {
-	return document.getElementById("FCarousel").offsetHeight;
-}
-
-function FCarouselWidth() {
-	return document.getElementById("FCarousel").offsetWidth;
-}
-
-function carouselElements(input) {
-	size = 0;
-	for (i in input) {
-		size = size + 1;
-	}
-	return size;
-}
-
-function carouselPages(input) {
-	size = carouselElements(input);
-	return Math.ceil(size / (rows * columns));
-}
-
-function buildpages() {
-	htmlstring = '\n';
-	pages = carouselPages(JSON);
-	for (p = 1; p <= pages; p = p + 1) {
-		htmlstring = htmlstring + '<div id="page' + p + '" class="page"></div>';
-	}
-	return htmlstring;
-}
-
-var FCarousel = {
-	/* initialize at 1 page */
-	numOfPages: 1,
-	/* initialize at page 1 */
-	currentPage: 1, 
+H2O.Carousel = function(options) {
+	var self = null;
+	var pages = null;
+	var data = null;
+	var currentPage = 1;
 	
-	preinitialize: function(JSONobject) {
-		/* grab data from JSON file */
-		JSON = JSONobject;
-		FCarousel.numOfPages = carouselPages(JSON);
-		/* build html */
-		$('#Holder').html(buildpages());
-		FCarousel.initialize();
-		FCarousel.resize();	
-	},
-    
-	initialize: function() {
+	(function() { // constructor
+		size = 0;
+		for (i in options.JSON) {
+			size = size + 1;
+		}
 		
-		/* Page number the item will live on*/
+		self = document.createElement('div');
+		self.setAttribute('id', options.id);
+		self.setAttribute('class', 'page'); 
+		self.setAttribute('style','\
+			width: 100%;\
+			height: 95%;\
+			overflow: hidden;\
+		');	
+		
+		holder = document.createElement('div');
+		holder.setAttribute('id', 'holder');
+		holder.setAttribute('style','\
+			height: 100%;\
+			left: 0px;\
+		');
+		
+		self.numOfPages = Math.ceil(size / (options.rowAmt * options.columnAmt)); // TODO: Make this private?
+
+		pages = []; // pages array
+		for (p = 1; p <= self.numOfPages; p = p + 1) {
+			x = document.createElement('div');
+			x.setAttribute('id', 'page' + p);
+			x.setAttribute('class', 'page'); 
+			x.setAttribute('style','float: left;');
+			pages[p] = x;
+			holder.appendChild(pages[p]);
+		};
+		
+		self.appendChild(holder);
+		
 		pageNum = 1;
-
-        for (i in JSON) {
-			
-			/* if page is full, go to next page */
-			if (i >= (rows * columns * pageNum)) {
+		
+		for (i in options.JSON) {
+			if (i >= (options.rowAmt * options.columnAmt * pageNum)) {
 				pageNum = pageNum + 1;
-			}
-
+			};
+		
             /* box */
             box = document.createElement('div');
             box.setAttribute('class', 'box');
 
-            /* a */
-            a = document.createElement('a');
-			
-			// TODO: Make this grab the link and load video and/or do some cool effect
-			//a.setAttribute('onclick', "Frescolita.StateManager.states['HOME'].startWidget('" + href + "')");
+			//             /* a */
+			//             a = document.createElement('a');
+			// 	
+			// // TODO: Make this grab the link and load video and/or do some cool effect
+			// //a.setAttribute('onclick', "Frescolita.StateManager.states['HOME'].startWidget('" + href + "')");
 
             /* icon */
             icon = document.createElement('div');
@@ -76,95 +63,107 @@ var FCarousel = {
 
             /* this loads slowly the first time. should preload. */
             img = document.createElement('img');
-            img.setAttribute('alt', JSON[i].title + " " + JSON[i].length); 
-            img.setAttribute('src', JSON[i].thumb); 
+            img.setAttribute('alt', options.JSON[i].title + " " + options.JSON[i].length); 
+            img.setAttribute('src', options.JSON[i].thumb); 
 
             icon.appendChild(img);
-            a.appendChild(icon);
-            box.appendChild(a);
-			if (pageNum > 1) {
-				//$('#page'+pageNum).append(box).hide();
-				$('#page'+pageNum).append(box);
-			} else {
-				$('#page'+pageNum).append(box);
-			}
-        };
-
-		$('.page').css({
-			'width' : FCarouselWidth(),
-			'height' : FCarouselHeight()
-		});
-		
-		$('#Holder').css("width",FCarouselWidth() * FCarousel.numOfPages);
-    },
-
-    resize: function() {
-
-		box_width_ratio = 1 / columns; // Width / # of Columns = Box Width
-		box_height_ratio = 1 / rows; // Height / # of Rows = Box Height
-
-        boxWidth = FCarouselWidth() * box_width_ratio;
-        boxHeight = FCarouselHeight() * box_height_ratio;
-        $(".box").width(boxWidth);
-        $(".box").height(boxHeight);
-
-        iconWidth = 0;
-        iconHeight = 0;
-        if (boxWidth >= boxHeight) {
-            /* Landscape */
-            iconWidth = boxHeight;
-            iconHeight = boxHeight;
-        }
-        else {
-            /* Portrait */
-            iconWidth = boxWidth;
-            iconHeight = boxWidth;
-        }
-
-        /* Padding */
-        iconWidth = iconWidth * (1 - 0.25);
-        iconHeight = iconHeight * (1 - 0.25);
-
-        /* Centering */
-        $(".icon").css("width", iconWidth);
-        $(".icon").css("height", iconHeight);
-        $(".icon").css("marginLeft", -iconWidth / 2);
-        $(".icon").css("marginTop", -iconHeight / 2);
-		
-		/* Reset each page size */
-		$('.page').css({
-			'width' : FCarouselWidth(),
-			'height' : FCarouselHeight()
-		});
-		/* Reset Holder width */
-		$('#Holder').css("width", FCarouselWidth() * FCarousel.numOfPages);
-		/* Reset Holder marginLeft */
-		$('#Holder').css("marginLeft", (FCarousel.currentPage - 1) * -FCarouselWidth());
-    },
+            //a.appendChild(icon);
+            //box.appendChild(a);
+			box.appendChild(icon);
+			pages[pageNum].appendChild(box);
+		}
 	
-	jumpToPage: function(page) {	
-		if (page != FCarousel.currentPage) {
+		self.addEventListener("DOMNodeInserted", function(e) {
+			console.log(self.parentNode);
+			if ((self.parentNode.id) === undefined) {
+				// the hell? it fires twice and the first time is no good.
+				// it's a DocumentFragment, from I don't know where
+			} else {
+				e.stopPropagation(); // cancel bubble
+				self.resize();
+				window.addEventListener("resize", self.resize, false);
+			}
+		}, false);
+	})();
+
+	self.getCurrentPage = function() {
+		return currentPage;
+	};
+	
+	self.resize = function() {
+		box_width_ratio = 1 / options.columnAmt; // Width / # of Columns = Box Width
+		box_height_ratio = 1 / options.rowAmt; // Height / # of Rows = Box Height
+
+        boxWidth = self.parentNode.offsetWidth * box_width_ratio;
+        boxHeight = self.parentNode.offsetHeight * box_height_ratio;
+		boxList = document.getElementsByClassName('box');
+		for (b = 0; b < boxList.length; b = b + 1) {
+			boxList[b].style.width = boxWidth;
+			boxList[b].style.height = boxHeight;
+		};
+
+		iconWidth = 0;
+		iconHeight = 0;
+		if (boxWidth >= boxHeight) {
+		    /* Landscape */
+		    iconWidth = boxHeight;
+		    iconHeight = boxHeight;
+		} else {
+		    /* Portrait */
+		    iconWidth = boxWidth;
+		    iconHeight = boxWidth;
+		};
+		
+		/* Padding */
+		iconWidth = iconWidth * (1 - 0.25);
+		iconHeight = iconHeight * (1 - 0.25);
+
+		/* Centering */
+		iconList = document.getElementsByClassName('icon');
+		for (i = 0; i < iconList.length; i = i + 1) {
+			iconList[i].style.width = iconWidth;
+			iconList[i].style.height = iconHeight;
+			iconList[i].style.marginLeft = -iconWidth / 2;
+			iconList[i].style.marginTop = -iconHeight / 2;
+		};
+
+		/* Reset each page size */
+		for (p in pages) {
+			pages[p].style.width = self.parentNode.offsetWidth;
+			pages[p].style.height = self.parentNode.offsetHeight;
+		};
+		
+		/* Reset Holder width */
+		holder.style.width = self.parentNode.offsetWidth * self.numOfPages;
+		holder.style.marginLeft = (self.currentPage - 1) * -self.parentNode.offsetWidth;
+	};
+	
+	self.jumpToPage = function(page) {	
+		if (page != self.currentPage) {
 			if (page < 1) {
 				alert("That page doesn't exist -> " + page);
-			} else if (page > FCarousel.numOfPages) {
+			} else if (page > self.numOfPages) {
 				alert("That page doesn't exist -> " + page);
 			} else {
 				$('#Holder').animate({
-					"marginLeft" : ((page - 1) * -FCarouselWidth())+"px"
+					"marginLeft" : ((page - 1) * -self.parentNode.offsetWidth)+"px"
 				}, 800);
 			};
-		} else if (page === FCarousel.currentPage) {
+		} else if (page === self.currentPage) {
 			// TODO: find better way to notify this
 			alert("You're already here!");
 		}
-		FCarousel.currentPage = page; // Change Page "State"
-	},
+		self.currentPage = page; // Change Page "State"
+	};
 	
-	nextPage: function(interval) { // Go forward interval number of pages
-		FCarousel.jumpToPage(FCarousel.currentPage + interval);
-	},
+	self.nextPage = function(interval) { // Go forward interval number of pages
+		self.jumpToPage(self.currentPage + interval); // TODO: Implied interval of 1
+	};
 	
-	prevPage: function(interval) { // Go backward interval number of pages
-		FCarousel.jumpToPage(FCarousel.currentPage - interval);
-	}
+	self.prevPage = function(interval) { // Go backward interval number of pages
+		self.jumpToPage(self.currentPage - interval);
+	};
+	
+	return self;
 };
+
