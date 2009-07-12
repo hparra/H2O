46864acd199c17f8ce9b8890b2006902dcfe161e
Hector G. Parra <hgparra+github@gmail.com>
2009-07-12 02:48:29 -0700

    H2O.Chart: Fundamental changes and corrections. Suggestions and TODOs in HGP comments.

--- src/js/H2O.Chart.js
+++ src/js/H2O.Chart.js
@@ -3,7 +3,7 @@
 // UC Irvine
 // 2009-07-02 YMD
 
-
+// HGP: My comments...
 H2O.Chart = function(options){
 	var self = null;
 	var xCoordinate = 25; // Time on graph
@@ -11,17 +11,25 @@ H2O.Chart = function(options){
 	var startPoint = 0; //option**
 	var absZero = 0;//option**
 	var yInterval = 25;//option**
-	var ctx;
+	var ctx; // HGP: Not necessary.
 	var maxSize;
 	var maxReading = 0;
 	var sizePercent = .7;//option**
 	var minReading = 99999999; //Change to integer max value
 	var totalReading = 0;
+	
+	// HGP: What is this?
 	var totalTime = 0; // Needs to be incremented
+
 	var onGraphY = 0;
+	
+	// HGP: Not anymore. IDs should be randomly generated if one isn't specified
 	var theName = ""; //option** REQUIRED TO BE INITALIZED BY USER/DEV (pass name reference)
+
+	// HGP: No.
 	var sizeXPixel = 700; //option**
 	var sizeYPixel = 300; //option**
+
 	var lineColor = "#fff";
 	var graphCel = 100; //option**
 	var colorBGBegin = "#330033"; //option**
@@ -34,6 +42,121 @@ H2O.Chart = function(options){
 
     /////********************** I IS PRIVATE FUNCTION ******************////////
 	
+	
+	// HGP: This should be in your demo HTML. See related comment in drawGraph()
+	getNumber = function(){
+		// Returns a number to be graphed
+		var temp = Math.ceil(Math.random()*100);
+		screenBuffer.push(temp);
+		return temp;
+	};
+
+
+	(function() {  //constructor
+
+		// Passing an ID is required for this object build properly.
+
+		// HGP: No.
+		//self = document.createElement('div');
+		//self.setAttribute('id', options.id);
+
+		self = document.createElement('canvas');
+		self.setAttribute('id', options.id);  // TODO: Error check or autogen
+		self.setAttribute('class', 'H2O_Chart');
+
+		theName = options.id; // HGP: Redundant now. Use self.id
+
+		
+		/*
+		if (typeof options.sizeX != undefined) {
+			sizeXPixel = options.sizeX;
+		}
+		if (typeof options.sizeY != undefined) {
+			sizeYPixel = options.sizeY;
+		}
+		*/
+	
+		if (typeof options.scale_percent != undefined) {
+			sizePercent = options.scale_percent / 100;
+		}
+		if( typeof options.dotPlot != undefined){
+			dotPlot = options.addDot; 	
+		}
+		if( typeof options.scale_graph != undefined){
+			resize = options.scale_graph;
+		}
+		onGraphY = sizeYPixel;
+		graphCel = options.graphCeil;
+		yInterval = options.intervalY;
+
+		// HGP: Again...
+		//ctx = document.getElementById(theName).getContext("2d"); // initialize ctx here
+		ctx = self.getContext("2d");
+
+		// HGP: Aha! This should scale too! The formula is up to you.
+		ctx.lineWidth = 3;
+
+		// HGP: Redundant. Called from resize().
+		//paintBG();	
+	
+		self.addEventListener("DOMNodeInserted", function(e) {
+			console.log(self.parentNode);
+			if ((self.parentNode.id) === undefined) {
+				// the hell? it fires twice and the first time is no good.
+				// it's a DocumentFragment, from I don't know where
+			} else {
+				e.stopPropagation(); // cancel bubble
+				resize();
+				window.addEventListener("resize", resize, false);
+			
+				// HGP: We start it once it's in the document
+				// we may even wait for the user/ui to start it
+				start();
+			}
+		}, false);
+	
+	})(); // End of constructor
+	
+	// HGP: Not anymore. My bad
+	/////********************** END OF PRIVATE FUNCTION ******************////////
+
+	// HGP: Not true. Part or all of this should be a public function
+	// We want to call draw from the outside, e.g. my_chart.draw(1337);
+	drawGraph = function(){
+		// This function draws the graph on to the canvas. Users will not needs to call this function.
+		++totalReading;
+		if (input < minReading) {
+			minReading = input;
+		}
+		if (input > maxReading) {
+			maxReading = input;
+		}
+		if (xCoordinate >= sizeXPixel - 5) {
+			xCoordinate = 25;
+			self.clear();
+		}
+		ctx.strokeStyle = '#fff'; // white
+		ctx.lineCap = 'round';
+		ctx.beginPath();
+		ctx.moveTo(xCoordinate, onGraphY);
+		xCoordinate += 15; //space between each read is 10px
+
+		// HGP: See comment above. This should be passed in.
+		// Painting and passing this data asynchronously is the tricky part!
+		// We need to differentiate between a chart that is always painting
+		// and one that paints only when it receives data.
+		input = getNumber(); // input read in here
+
+		percentOnGraph = input / graphCel;
+		onGraphY = Math.round(sizeYPixel * percentOnGraph);
+		onGraphY = sizeYPixel - onGraphY;
+		ctx.lineTo(xCoordinate, onGraphY);
+		if(dotPlot === true){
+			ctx.arc(xCoordinate, onGraphY, 2, 0, Math.PI*2, true);
+		}
+		ctx.stroke();
+	};
+	/// End of drawgraph ///
 
 	paintBG = function(){
 		//This function will re-draw the background along with the lines
@@ -54,7 +177,10 @@ H2O.Chart = function(options){
 			percent = (Math.round(graphCel / numberOfLines) * k) / graphCel;
 			percent = sizeYPixel * percent;
 			percent = sizeYPixel - percent;
-			ctx.fillText(Math.round(graphCel / numberOfLines) * k, 1, percent);
+
+			// HGP: This is too new. An option perhaps?
+			//ctx.fillText(Math.round(graphCel / numberOfLines) * k, 1, percent);
+
 			k++;
 			ctx.fillStyle = lineargradient;
 			ctx.lineTo(sizeXPixel, i);
@@ -64,109 +190,45 @@ H2O.Chart = function(options){
 		ctx.lineWidth = 3;
 		ctx.strokeStyle = '#fff'; // white
 	};
-	
-	
-	getNumber = function(){
-		// Returns a number to be graphed
-		var temp = Math.ceil(Math.random()*100);
-		screenBuffer.push(temp);
-		return temp;
-	};
-	start = function(){
-	  return setInterval(drawGraph, 100);
-	};
-
-	drawGraph = function(){
-		// This function draws the graph on to the canvas. Users will not needs to call this function.
-		++totalReading;
-		if (input < minReading) {
-			minReading = input;
-		}
-		if (input > maxReading) {
-			maxReading = input;
-		}
-		if (xCoordinate >= sizeXPixel - 5) {
-			xCoordinate = 25;
-			self.clear();
-		}
-		ctx.strokeStyle = '#fff'; // white
-		ctx.lineCap = 'round';
-		ctx.beginPath();
-		ctx.moveTo(xCoordinate, onGraphY);
-		xCoordinate += 15; //space between each read is 10px
-		input = getNumber(); // input read in here
-		percentOnGraph = input / graphCel;
-		onGraphY = Math.round(sizeYPixel * percentOnGraph);
-		onGraphY = sizeYPixel - onGraphY;
-		ctx.lineTo(xCoordinate, onGraphY);
-		if(dotPlot === true){
-			ctx.arc(xCoordinate, onGraphY, 2, 0, Math.PI*2, true);
-		}
-		ctx.stroke();
-	};
-	/// End of drawgraph ///
 
 
-	(function() {  //constructor
-
-	// Passing an ID is required for this object build properly.
-	self = document.createElement('div');
-	self.setAttribute('id', options.id);
-	theName = options.id;
-	if (typeof options.sizeX != undefined) {
-		sizeXPixel = options.sizeX;
-	}
-	if (typeof options.sizeY != undefined) {
-		sizeYPixel = options.sizeY;
-	}
-	if (typeof options.scale_percent != undefined) {
-		sizePercent = options.scale_percent / 100;
-	}
-	if( typeof options.dotPlot != undefined){
-		dotPlot = options.addDot; 	
-	}
-	if( typeof options.scale_graph != undefined){
-		resize = options.scale_graph;
-	}
-	onGraphY = sizeYPixel;
-	graphCel = options.graphCeil;
-	yInterval = options.intervalY;
-	ctx = document.getElementById(theName).getContext("2d"); // initialize ctx here
-	ctx.lineWidth = 3;
-	paintBG();	
-	start();
+	// HGP: we may actually want this to be public
+	start = function() {
+		return setInterval(drawGraph, 100);
+	};	
 	
-	self.addEventListener("DOMNodeInserted", function(e) {
-		console.log(self.parentNode);
-		if ((self.parentNode.id) === undefined) {
-			// the hell? it fires twice and the first time is no good.
-			// it's a DocumentFragment, from I don't know where
-		} else {
-			e.stopPropagation(); // cancel bubble
-				self.resize();
-			window.addEventListener("resize", self.resize, false);
-		}
-	}, false);
-	//end resize
+	// HGP: we should have a self.stop too
 	
-	})(); // End of constructor
-	
-	/////********************** END OF PRIVATE FUNCTION ******************////////
-
-		
-	
-		//resize
-		self.resize = function(){
+	// HGP: Closure DOES allow resize to be a private function
+	// even though we are telling window to call it
+	// I don't know why it wasn't working before!
+	resize = function(){
 		if (resize) {
-			var w = window.innerWidth;
-			var h = window.innerHeight;
-			sizeXPixel = Math.ceil(w * sizePercent);
-			sizeYPixel = Math.ceil(h * sizePercent);
-			var c = document.getElementById(theName);
-			c.height = sizeYPixel;
-			c.width = sizeXPixel;
+			// HGP: It should use the size of the parent
+			//var w = window.innerWidth;
+			//var h = window.innerHeight;
+			var w = self.parentNode.offsetWidth;
+			var h = self.parentNode.offsetHeight;
+			
+			// HGP: Don't abstract this, use the value directly from the dom
+			//sizeXPixel = Math.ceil(w * sizePercent);
+			//sizeYPixel = Math.ceil(h * sizePercent);
+			sizeXPixel = Math.ceil(w);
+			sizeYPixel = Math.ceil(h);
+
+			// HGP: You already have access to the canvas
+			//var c = document.getElementById(theName);
+			//c.height = sizeYPixel;
+			//c.width = sizeXPixel;
+			self.height = sizeYPixel;
+			self.width = sizeXPixel;
+
+			// HGP: Those six lines above can be two
+
 			xCoordinate = 25;
-			paintBG();
+			paintBG(); // HGP: Good.
+			
+			// HGP: We may want to abstact this out
 			percentOnGraph = startPoint / graphCel;
 			onGraphY = Math.ceil(sizeYPixel * percentOnGraph);
 			onGraphY = sizeYPixel - onGraphY;
@@ -196,6 +258,7 @@ H2O.Chart = function(options){
 	// Returns the min reading
 	self.getMinReading = function(){return minReading;};
 	
+	// HGP: In ms? This can't be trusted due to one-threaded nature of JS
 	// Returns the total time since first reading
 	self.getTotalTime = function(){return totalTime;};
 	
@@ -212,4 +275,4 @@ H2O.Chart = function(options){
 	/// End of graph clearing ///
 
 	return self;
-};
+}; // HGP: Great work!
