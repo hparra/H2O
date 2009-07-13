bde9cd01a3bc030844c7790927b17706e07665b3
Hector G. Parra <hgparra+github@gmail.com>
2009-07-12 20:41:31 -0700

    H2O.Label: Now resizes as intended, with width and height bounds, but buggy here and there.

--- src/js/H2O.Label.js
+++ src/js/H2O.Label.js
@@ -1,4 +1,6 @@
-
+/**
+ * 
+ */
 // USE: my_label = new H2O.Label()
 H2O.Label = function(options) {
 	var self = null;
@@ -6,43 +8,51 @@ H2O.Label = function(options) {
 	(function() { // constructor
 		
 		// check for options, if not create defaults
-		//options.id = options.id || "testID";
+		options.id = options.id || "testID"; // FIXME: Should be random number
 		//options.text ||= "testTEXT";
 
-		self = document.createElement('div'); // TODO: should we use P or H2 instead for accessibility?
-		self.setAttribute('id', options.id); // TODO: assign random id if not present
-		//label.setAttribute('class' ''); // TODO: accept classes as options
+		self = document.createElement('span'); // TODO: should we use P or H2 instead for accessibility?
+		self.setAttribute('id', options.id);
+		self.setAttribute('class', 'H2O_Label');
 		self.setAttribute('style','\
-			display: block;\
-			margin: 0;\
-			padding: 0;\
+			margin: 0px;\
+			padding: 0px;\
 			overflow: hidden;\
+			font-size: 14px;\
 		');
 		self.innerHTML = options.text;
 		
 		self.addEventListener("DOMNodeInserted", function(e) { // NOTE: Not supported in IE
-			console.log(self.parentNode);
-			if ((self.parentNode.id) === undefined) {
-				// the hell? it fires twice and the first time is no good.
-				// it's a DocumentFragment, from I don't know where
-			}
-			else {
+			//console.log(self.parentNode);
+			// the hell? it fires twice and the first time is no good.
+			// it's a DocumentFragment, from I don't know where
+			if ((self.parentNode.id) !== undefined) {
 				e.stopPropagation(); // cancel bubble
-				self.resize();
-				window.addEventListener("resize", self.resize, false);
+				resize();
+				window.addEventListener("resize", resize, false);
 			}
 		}, false);
 	})();
 
-	self.resize = function() {
+	resize = function() {
+		
 		self.style.width = self.parentNode.offsetWidth;
 		self.style.height = self.parentNode.offsetHeight;
-		self.style.lineHeight = self.parentNode.offsetHeight + "px";
-		self.style.fontSize = self.parentNode.offsetHeight + "px";
+		
+		width_ratio = self.parentNode.offsetWidth / self.offsetWidth;
+		fontSize = self.style.fontSize.replace(/px/, ""); // FIXME: assumes we are only using px
+		self.style.lineHeight = self.parentNode.offsetHeight + "px"; // always correct
+		self.style.fontSize = (fontSize * width_ratio) + "px";
+		
+		height_ratio = self.parentNode.offsetHeight / self.offsetHeight;
+		if (self.parentNode.offsetHeight < self.offsetHeight) {
+			fontSize = self.style.fontSize.replace(/px/, "");
+			self.style.fontSize = (fontSize * height_ratio) + "px";
+		}
 	}
 
 	self.destroy = function() {
-		window.removeEventListener("resize", self.resize);
+		window.removeEventListener("resize", resize);
 		// todo: remove element
 		self = null;
 	};
