7426a64e036b426b58ecf4776b13607de27f7791
Hector G. Parra <hgparra+github@gmail.com>
2009-08-11 02:33:50 -0700

    Proper H2O object and reverted Carousel function test location.

--- public/src/js/H2O.js
+++ public/src/js/H2O.js
@@ -1,10 +1,8 @@
 /**
  * H2O
  */
-//(function() {
-	
-	if (typeof H2O === "undefined") {
-		var H2O = {};
+(function() {
+	if (typeof window.H2O === "undefined") {
+		var H2O = window.H2O = {};
 	}
-	
-//})();
+})();
