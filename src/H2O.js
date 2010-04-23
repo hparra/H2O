240aa770608362e727b660523eb2b1a4fb179431
Hector G. Parra <hgparra+github@gmail.com>
2010-04-23 02:50:00 -0700

    initializeTree() added but not tested.

--- src/H2O.js
+++ src/H2O.js
@@ -26,6 +26,62 @@
 	};
 	
 	/**
+	 * Initializes an Element (sub)tree for H2O compliance
+	 * @private
+	 *///
+	initializeTree = function(node) {
+		
+		/* bypass non-Elements (and children) */
+		if (node.nodeType != Node.ELEMENT_NODE) return;
+		
+		/* bypass Elements (and their children) with "NO_H2O" class */
+		if (node.className.match(/NO_H2O/)) return; /* Too new HTML5/DOM3: node.classList.contains("NO_H2O"); */
+		
+		/* initalize elements by tag */
+		switch (node.tagName) {
+		case "IMG":
+			H2O.Image(node); break;
+		case "H1":
+		case "H2":
+		case "H3":
+		case "H4":
+		case "H5":
+		case "H6":
+			H2O.Label(node); break;
+		case "OL":
+		case "UL":
+			H2O.List(node); break;
+		case "DIV":
+		case "SPAN":
+		case "VIDEO":
+		default:
+			/* DO NOTHING */ break;
+		}
+		
+		/* define dispatchResizeEvent() if it doesn't exist (it shouldn't) */
+		if (typeof node.dispatchResizeEvent != "function") {
+			node.dispatchResizeEvent = function() {
+				var e = document.createEvent("Event");
+				e.initEvent("resize", false, false);
+				node.dispatchEvent(e);
+			};
+		}
+		
+		/* define node.resize() if it doesn't exist (it may) */
+		if (node.parentNode != document.body) {
+			if (typeof node.resize != "function") {
+				node.resize = node.dispatchResizeEvent;
+			}
+			node.parentNode.addEventListener("resize", node.resize, false);
+		}
+		
+		/* preorder depth-first traversal */
+		for (var int i = 0; i < node.childNodes.length; ++i) {
+			initializeTree(node.childNodes[i]);
+		}
+	};
+	
+	/**
 	 * Adds ParentResizeEvent listeners/dispatchers and converts all .H2O elements
 	 * @private
 	 *///
